1f297975ba1c997df518403a6fbaaca8af4427bf
Pablo Alcain <pabloalcain@gmail.com>
2016-05-18 02:38:04 -0300

    Agregada nueva versión de salarios

--- salarios.py
+++ salarios.py
@@ -1,141 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+Módulo básico (por ahora sólo funcional) para realizar los gráficos
+"""
 import pandas as pd
 import numpy as np
-from bokeh.io import output_server, vplot
 import bokeh.models as bkm
-from bokeh.plotting import figure, output_file, show
-from bokeh.charts import Line
+import bokeh.plotting as bkp
 
-import datetime
+def proyectar_salarios(salario, fecha_comienzo, fecha_fin, oferta):
+  """
+  Función que genera una proyección sobre el salario.
 
-ipc = pd.read_csv('ipc_sanluis.csv', parse_dates=['fecha'])
-ipc['ipc_sanluis'] = 100 * np.cumprod(ipc['ipc_sanluis']/100 + 1)
-salario = pd.read_csv('salarios_docentes.csv', parse_dates=['fecha'])
-salario['docentes UBA'] = 100 * salario['docentes UBA']/salario['docentes UBA'][0]
-ultimo = salario['fecha'].values[-1]
-fin_paritarias = pd.Timestamp('2017-03-01')
-agregado = pd.date_range(freq='m', start=pd.Timestamp(ultimo), end=fin_paritarias)
-agregado += pd.Timedelta('1 day') - pd.Timedelta('1 M')
-oferta = [[(pd.Timestamp('2016-05-01'), 18), (pd.Timestamp('2016-11-01'), 15)],]
+  Parámetros
+  ----------
 
-a = oferta[0]
+  salario : float
+      El último salario dato
 
-proyectado = pd.DataFrame({'fecha': agregado})
-s = np.zeros_like(proyectado.values, dtype=np.float).flatten()
+  fecha_comienzo : pandas Timestamp
+      Fecha de comienzo de proyección (usualmente coincidente con la
+      fecha del último salario dato)
 
-for i, mes in enumerate(proyectado['fecha']):
-  if i == 0:
-    s[i] = salario['docentes UBA'].values[-1]
-    continue
-  b = False
-  for _ in a:
-    if mes == _[0]:
-      print mes
-      s[i] = s[i-1] * (1 + _[1]/100)
-      b = True
-      continue
-  if b: continue
-  s[i] = s[i-1]
+  fecha_fin : pandas Timestamp
+      Última fecha contemplada por paritaria
 
+  oferta : List
+      Una lista de tuplas, donde el primer elemento de la tupla es la
+      fecha en la que se cobra el aumento y la segunda es el
+      porcentaje de aumento
 
+  Returns
+  -------
 
+  proyectado : pandas DataFrame
+      Un Dataframe con fechas y valores de la proyección
+  """
+  fechas = pd.date_range(freq='m', start=fecha_comienzo, end=fecha_fin)
+  fechas += pd.Timedelta('1 day') # Porque si no da el último día del mes
+  s = np.zeros_like(fechas.values, dtype=np.float).flatten()
 
-output_file("example.html", title="example")
+  for i, mes in enumerate(fechas):
+    if i == 0:
+      s0 = salario
+    else:
+      s0 = s[i-1]
+    b = False
+    for of in oferta:
+      if mes == of[0]:
+        s[i] = s0 * (1 + of[1]/100.0)
+        b = True
+        continue
+    if not b: s[i] = s0
 
+  proyectado = pd.DataFrame({'fecha': fechas,
+                             'salario': s})
+  return proyectado
 
+def proyectar_canasta(canasta, fecha_comienzo, fecha_fin, porcentaje):
+  """
+  Función que genera una proyección sobre el costo de vida.
 
+  Parámetros
+  ----------
 
+  canasta : float
+      La última canasta
 
-TOOLS = [bkm.PanTool(),
-         bkm.WheelZoomTool(),
-         bkm.BoxZoomTool(),
-         bkm.HoverTool(tooltips=[('Fecha', '$x'), ('Monto', '$y')])]
+  fecha_comienzo : pandas Timestamp
+      Fecha de comienzo de proyección (usualmente coincidente con la
+      fecha de la última canasta dato)
 
+  fecha_fin : pandas Timestamp
+      Última fecha contemplada por proyección de inflación
 
+  porcentaje : float
+      Porcentaje de inflación mensual
 
-s1 = figure(title='primero', x_axis_type='datetime', tools=TOOLS)
+  Returns
+  -------
 
-s1.line(ipc['fecha'], ipc['ipc_sanluis'], color='navy',
-        legend='Costo de Vida', line_width=2)
-
-s1.circle(ipc['fecha'], ipc['ipc_sanluis'], color='navy', size=8)
-
-s1.line(salario['fecha'], salario['docentes UBA'], color='green',
-        legend='Salario', line_width=2)
-
-s1.circle(salario['fecha'], salario['docentes UBA'], color='green', size=8)
-
-TOOLS = [bkm.PanTool(),
-         bkm.WheelZoomTool(),
-         bkm.BoxZoomTool(),
-         bkm.HoverTool(tooltips=[('Fecha', '$x'), ('Monto', '$y')]),]
-
-s2 = figure(title='primero', x_axis_type='datetime', tools=TOOLS)
-s2.line(ipc['fecha'], salario['docentes UBA']/ipc['ipc_sanluis'], color='navy')
-
-p = vplot(s1, s2)
-show(p)
-
-"""
-numeroDeMesesEtimados = 8
-# Hacemos el indice acumulado de inflacion. Se considera el mes de septiembre de 2011 como la referencia y que la inflacion aplica al costo de vida del mes siguiente
-InflacionReferida = [100]
-for inflacion in IPC_SanLuis:
-    InflacionReferida = InflacionReferida + [InflacionReferida[-1]*(1+inflacion/100)]
-
-InflacionReferida = InflacionReferida[1:]
-
-#Separamos los datos hasta donde hay datos de inflacion
-indice_FechasSinProyeccion=indice_Fechas[:-numeroDeMesesEtimados]
-InflacionReferidaSinProyeccion = InflacionReferida[:-numeroDeMesesEitmados]
-sueldoReferidoSinProyeccion = sueldoReferido[:-numeroDeMesesEtimados]
-SalarioRealSinProyeccion = [(dato[0]/dato[1]-1)*100 for dato in zip (sueldoReferidoSinProyeccion,InflacionReferidaSinProyeccion)]
-sueldoReferidoProyeccionActual = sueldoReferido[-numeroDeMesesEtimados:]
+  proyectado : pandas DataFrame
+      Un Dataframe con fechas y valores de la proyección
+  """
+  fechas = pd.date_range(freq='m', start=fecha_comienzo, end=fecha_fin)
+  fechas += pd.Timedelta('1 day') # Porque si no da el último día del mes
+  s = np.zeros_like(fechas.values, dtype=np.float).flatten()
 
 
+  for i, _ in enumerate(fechas):
+    if i == 0:
+      s0 = canasta
+    else:
+      s0 = s[i-1]
+    s[i] = s0 * (1 + porcentaje/100.0)
 
-# Hacemos tres proyecciones para abril en adelante, una con 3% de inflacion, otra con 1,5 y otra con 0.5
-proyeccion1 = 3.
-proyeccion2 = 2.
-proyeccion3 = 0
+  proyectado = pd.DataFrame({'fecha': fechas,
+                             'canasta': s})
+  return proyectado
 
-indiceFechasProyeccion = indice_Fechas[-numeroDeMesesEtimados:]
-InflacionProyeccion_1 = [InflacionReferidaSinProyeccion[-1]]
-InflacionProyeccion_2 = [InflacionReferidaSinProyeccion[-1]]
-InflacionProyeccion_3 = [InflacionReferidaSinProyeccion[-1]]
-for value in indiceFechasProyeccion:
-    InflacionProyeccion_1 = InflacionProyeccion_1 + [InflacionProyeccion_1[-1]*(1+proyeccion1/100)]
-    InflacionProyeccion_2 = InflacionProyeccion_2 + [InflacionProyeccion_2[-1]*(1+proyeccion2/100)]
-    InflacionProyeccion_3 = InflacionProyeccion_3 + [InflacionProyeccion_3[-1]*(1+proyeccion3/100)]
+def crear_figura(canasta, salario, proyeccion_canasta, proyeccion_salario,
+                 estilo='lin'):
+  """
+  Crea una figura de bokeh
 
-SalarioRealProyeccion_1 = [(dato[0]/dato[1]-1)*100 for dato in zip (sueldoReferidoProyeccionActual,InflacionProyeccion_1)]
-SalarioRealProyeccion_2 = [(dato[0]/dato[1]-1)*100 for dato in zip (sueldoReferidoProyeccionActual,InflacionProyeccion_2)]
-SalarioRealProyeccion_3 = [(dato[0]/dato[1]-1)*100 for dato in zip (sueldoReferidoProyeccionActual,InflacionProyeccion_3)]
+  Parámetros
+  ----------
 
+  canasta : pandas DataFrame
+      La canasta a considerar
 
+  salario : pandas DataFrame
+      El salario histórico
 
-evoluciones, ax = plt.subplots(figsize=(20, 10))
-ind = np.arange(len(sueldoReferido))
-plt.plot(sueldoReferidoSinProyeccion, label='Evolucion sueldo')
-plt.plot(ind[-numeroDeMesesEtimados:]-1,sueldoReferidoProyeccionActual, label='propuesta actual paritaria (15+15)')
-plt.plot(ind[-numeroDeMesesEtimados-1:],InflacionProyeccion_1, label='proyeccion inflacion '+str(proyeccion1)+'% mensual (actual)')
-plt.plot(ind[-numeroDeMesesEtimados-1:],InflacionProyeccion_2, label='proyeccion inflacion '+str(proyeccion2)+'% mensual (generosa)')
-plt.plot(ind[-numeroDeMesesEtimados-1:],InflacionProyeccion_3, label='proyeccion inflacion '+str(proyeccion3)+'% mensual (necesaria)')
-plt.plot(InflacionReferidaSinProyeccion, label='Evolucion costo de vida')
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.grid(True)
-plt.xticks(ind, indice_Fechas, rotation='vertical')
+  proyeccion_salario : pandas DataFrame
+      La proyección del salario con la propuesta de paritarias
 
-comparativo = plt.figure(figsize=(20, 10))
-referencia = [0 for value in sueldoReferido]
+  proyeccion_canasta : pandas DataFrame
+      La proyección de la canasta básica
 
-plt.plot(SalarioRealSinProyeccion, label='Poder adquisitivo respecto a Octubre 2011')
-plt.plot(ind[-numeroDeMesesEtimados:]-1,SalarioRealProyeccion_1, label='Poder adquisitivo propuesta 15+15 proyeccion inflacion '+str(proyeccion1)+'%)')
-plt.plot(ind[-numeroDeMesesEtimados:]-1,SalarioRealProyeccion_2, label='Poder adquisitivo propuesta 15+15 proyeccion inflacion '+str(proyeccion2)+'%)')
-plt.plot(ind[-numeroDeMesesEtimados:]-1,SalarioRealProyeccion_3, label='Poder adquisitivo propuesta 15+15 proyeccion inflacion '+str(proyeccion3)+'%)')
-plt.plot(referencia, 'r--', label='Referencia')
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.xticks(ind, indice_Fechas, rotation='vertical')
+  estilo : {'lin', 'log'}
+      Estilo lineal o logarítmico del gráfico
+
+  Returns
+  -------
+
+  fig : bokeh Figure
+      La figura que resulta
+  """
+
+  TOOLS = [bkm.PanTool(),
+           bkm.WheelZoomTool(),
+           bkm.BoxZoomTool(),
+           bkm.HoverTool(tooltips=[('Fecha', '$x'), ('Monto', '$y')])]
 
 
-plt.show()
-"""
+  if estilo == 'lin':
+    fig = bkp.figure(title='Salario normalizado',
+                     x_axis_type='datetime', tools=TOOLS)
+  elif estilo == 'log':
+    fig = bkp.figure(title='Salario normalizado',
+                     x_axis_type='datetime', tools=TOOLS,
+                     y_axis_type='log')
+  else:
+    raise ValueError('Opción {0} no encontrada'.format(estilo))
+
+
+
+  fig.line(canasta['fecha'], canasta['ipc'], color='navy',
+           legend='Costo de Vida (IPC San Luis)', line_width=2)
+
+  fig.circle(canasta['fecha'], canasta['ipc'], color='navy', size=8)
+
+  fig.line(salario['fecha'], salario['salario'], color='green',
+           legend='Salario', line_width=2)
+
+  fig.circle(salario['fecha'], salario['salario'], color='green', size=8)
+
+  fig.line(proyeccion_salario['fecha'], proyeccion_salario['salario'], color='red',
+           legend='Paritarias Salario', line_width=2, line_dash='dashed')
+
+  fig.line(proyeccion_canasta['fecha'], proyeccion_canasta['canasta'],
+           color='black', legend='Proyección Costo de Vida', line_width=2,
+           line_dash='dashed')
+
+  fig.legend[0].location = 'top_left'
+
+  return fig
